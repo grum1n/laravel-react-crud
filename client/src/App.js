c5e5608f1bb360f8366c4afcc238f2e69eb4bf87
grum1n <grumin.el.pastas@gmail.com>
2022-12-28 15:44:25 +0200

    added code to ShowProducts.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
     return (
       <div className='App'>
-        <button className='btn btn-primary'>Create</button>
+        {/* <button className='btn btn-primary'>Create</button> */}
       </div>
     )
 }
